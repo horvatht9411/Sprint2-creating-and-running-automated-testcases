53709c272348c96aae12d8b92ef91a8279c4481c
horvatht9411 <horvatht9411@gmail.com>
2022-10-24 13:40:40 +0200

    feat: start login

--- src/test/java/TestLogin.java
+++ src/test/java/TestLogin.java
@@ -1,18 +1,51 @@
 import com.codecool.App;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestLogin {
 
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    Logger logger = LoggerFactory.getLogger(App.class);
+    WebDriver webDriver;
+    WebElement loginButton;
+    WebElement userName;
+    WebElement password;
+    String url = "https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa";
 
-
-    @Test
-    public void start(){
-        logger.info("Logger is cool!");
+    @BeforeEach
+    void init() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = Util.visibilitySetup();
+        webDriver.manage().window().maximize();
+        webDriver.get(url);
+        userName = webDriver.findElement(By.id("login-form-username"));
+        password = webDriver.findElement(By.id("login-form-password"));
+        loginButton = webDriver.findElement(By.id("login"));
     }
 
+    @AfterEach
+    void close() {
+
+    }
+
+    @Test
+    @DisplayName("Correct username and password")
+    public void correctCredential() {
+        userName.sendKeys(System.getProperty("username"));
+        password.sendKeys((System.getProperty("password")));
+        loginButton.click();
+        webDriver.findElement(By.id("user-options-content")).click();
+        String logOut = webDriver.findElement(By.id("log_out")).getText();
+        Assert.assertEquals("Log Out", logOut);
+    }
 }
 
 
---
+++ src/test/java/Util.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Util {
+    static boolean isVisible = false;
+
+
+
+    static void executeScript(WebElement webElement, JavascriptExecutor executor) {
+        executor.executeScript("arguments[0].click();", webElement);
+    }
+
+    static boolean checkModalVisibility(WebDriver webDriver){
+        var modal = webDriver.findElements(By.cssSelector("body > div.fade.modal.show > div > div"));
+        boolean exist = false;
+        if (!modal.isEmpty()){
+            if (modal.get(0).isDisplayed()) {
+                String modalHeader = webDriver.findElement(By.id("example-modal-sizes-title-lg")).getText();
+                if("Thanks for submitting the form".equals(modalHeader)) exist = true;
+            }}
+        return exist;
+    }
+
+    static WebDriver visibilitySetup(){
+        WebDriver webDriver;
+        if (!isVisible) {
+            // Run in background
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            webDriver = new ChromeDriver(options);
+        } else {
+            //Open browse
+            webDriver = new ChromeDriver();
+        }
+        return webDriver;
+    }
+
+}
