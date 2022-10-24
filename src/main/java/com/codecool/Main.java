b6e527cecdbd35d5a4fb7f02f763c0a4e0f238fd
horvatht9411 <horvatht9411@gmail.com>
2022-10-24 11:29:28 +0200

    add: log properties

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,6 +1,12 @@
 package com.codecool;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/test/java/TestLogin.java
@@ -0,0 +1,18 @@
+import com.codecool.Main;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogin {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
+    @Test
+    void start(){
+        logger.info("Logger is cool!");
+    }
+
+}
+
+
