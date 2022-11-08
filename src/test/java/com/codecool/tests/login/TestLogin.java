391ae7dde9df52ccbd0f6296cdea74ed069cc0da
Hladgerd <hladgerd@gmail.com>
2022-11-08 14:56:08 +0100

    refact: reformat

--- src/test/java/com/codecool/tests/login/TestLogin.java
+++ src/test/java/com/codecool/tests/login/TestLogin.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
