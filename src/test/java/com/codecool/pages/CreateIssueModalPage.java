d5f7017f9c5976c97ff6e1834ec3757e44d0cc38
Hladgerd <hladgerd@gmail.com>
2022-11-09 13:39:11 +0100

    feat: add create issue modal locators and action methods

---
+++ src/test/java/com/codecool/pages/CreateIssueModalPage.java
@@ -0,0 +1,95 @@
+package com.codecool.pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.UUID;
+
+public class CreateIssueModalPage {
+    WebDriver webDriver;
+
+    @FindBy(xpath = "//*[@id='create-issue-dialog']")
+    public WebElement issueModal;
+
+    @FindBy(xpath = "//*[@id='project-field']")
+    public WebElement projectSelector;
+
+    @FindBy(xpath = "//*[@id='summary']")
+    public WebElement summary;
+
+    @FindBy(xpath = "//*[@id='create-issue-submit']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//*[@id='aui-flag-container']//a")
+    public WebElement newIssueLink;
+
+    @FindBy(xpath = "//*[@id='opsbar-operations_more']")
+    public WebElement moreMenu;
+
+    @FindBy(xpath = "//*[@id='delete-issue']/a/span")
+    public WebElement delete;
+
+    @FindBy(xpath = "//*[@id='delete-issue-dialog']")
+    public WebElement deleteIssueDialog;
+
+    @FindBy(xpath = "//*[@id='delete-issue-submit']")
+    public WebElement deleteIssueSubmit;
+
+    @FindBy(xpath = "//*[@id='summary']/following::*")
+    public WebElement warningMessageToFillSummary;
+
+    @FindBy(xpath = "//[@id='create-issue-submit']/following::*")
+    public WebElement cancelButton;
+
+    public CreateIssueModalPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void fillUpProjectName(String projectName) {
+        projectSelector.click();
+        projectSelector.sendKeys(Keys.BACK_SPACE);
+        projectSelector.sendKeys(projectName);
+        projectSelector.sendKeys(Keys.ENTER);
+    }
+
+    public void fillUpSummary() {
+        String value = UUID.randomUUID().toString();
+        summary.sendKeys(value);
+    }
+
+    public String getSummaryText() {
+        return summary.getText();
+    }
+
+    public void submitNewIssue() {
+        submitButton.click();
+    }
+
+    public void clickOnNewIssueLink() {
+        newIssueLink.click();
+    }
+
+    public void deleteNewlyCreatedIssue(WebDriverWait wait) {
+        moreMenu.click();
+        delete.click();
+        wait.until(ExpectedConditions.visibilityOf(deleteIssueDialog));
+        deleteIssueSubmit.click();
+    }
+
+    public String getWarningMessageToFillSummary() {
+        return warningMessageToFillSummary.getText();
+    }
+
+    public void closeCreateModal(WebDriverWait wait) {
+        cancelButton.click();
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = webDriver.switchTo().alert();
+        alert.accept();
+    }
+
+
+}
