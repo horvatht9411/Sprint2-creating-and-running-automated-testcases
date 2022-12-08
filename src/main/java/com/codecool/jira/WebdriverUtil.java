1ebd6ec9e6f339a6ee0ff0ce7a1a4092e1ea348b
Vitályos Márton <marci12345678956@gmail.com>
2022-12-08 10:14:45 +0100

    refact: delete rows from WebdriverUtil

--- src/main/java/com/codecool/jira/WebdriverUtil.java
+++ src/main/java/com/codecool/jira/WebdriverUtil.java
@@ -25,8 +25,8 @@ public class WebdriverUtil {
 
     private static WebdriverUtil INSTANCE;
 
-    private WebdriverUtil(){
-        if (Boolean.parseBoolean(Util.readProperty("local"))){
+    private WebdriverUtil() {
+        if (Boolean.parseBoolean(Util.readProperty("local"))) {
             System.setProperty("remoteBrowser", Util.readProperty("remoteBrowser"));
             webDriver = setupWebdriver();
         } else {
@@ -35,19 +35,9 @@ public class WebdriverUtil {
         webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(SECONDS));
     }
 
-//    private void setSystemProperties() {
-//        System.setProperty("username", Util.readProperty("Pusername"));
-//        System.setProperty("password", Util.readProperty("Ppassword"));
-//        System.setProperty("username", Util.readProperty("Pusername"));
-//        System.setProperty("headless", Util.readProperty("Pheadless"));
-//        System.setProperty("remoteBrowser", Util.readProperty("PremoteBrowser"));
-//        System.setProperty("localConnection", Util.readProperty("PlocalConnection"));
-//        System.setProperty("url", Util.readProperty("Purl"));
-//
-//    }
 
-    public static WebdriverUtil getInstance(){
-        if(INSTANCE == null) {
+    public static WebdriverUtil getInstance() {
+        if (INSTANCE == null) {
             INSTANCE = new WebdriverUtil();
         }
 
@@ -62,24 +52,24 @@ public class WebdriverUtil {
         return webDriverWait;
     }
 
-    private WebDriver setupWebdriver(){
-            WebDriverManager.chromedriver().setup();
-            if (headless) {
-                // Run in background
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("headless");
-                webDriver = new ChromeDriver(options);
-            } else {
-                //Open browser
-                webDriver = new ChromeDriver();
-                webDriver.manage().window().maximize();
-            }
-            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    private WebDriver setupWebdriver() {
+        WebDriverManager.chromedriver().setup();
+        if (headless) {
+            // Run in background
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            webDriver = new ChromeDriver(options);
+        } else {
+            //Open browser
+            webDriver = new ChromeDriver();
+            webDriver.manage().window().maximize();
+        }
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 //        webDriver.get(BasePage.LOGIN_URL);
         return webDriver;
     }
 
-    private WebDriver setupRemoteWebdriver(){
+    private WebDriver setupRemoteWebdriver() {
         String remoteBrowser = Util.readProperty("remoteBrowser");
         String password = Util.readProperty("password");
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -97,7 +87,7 @@ public class WebdriverUtil {
         }
     }
 
-    public void shutDown(){
+    public void shutDown() {
         webDriver.quit();
         INSTANCE = null;
     }
